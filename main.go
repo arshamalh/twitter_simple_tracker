d07d79b73100fbac3773916e36634a4a63d8f4c8
arshamalh <arshamalh@gmail.com>
2022-05-11 19:32:42 +0430

    Fiber stop tracking users and tweets added + some improvements.

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/arshamalh/twigo"
+	"github.com/arshamalh/twigo/entities"
 	"github.com/go-co-op/gocron"
 	"github.com/gofiber/fiber/v2"
 	"github.com/sdomino/scribble"
@@ -17,25 +18,26 @@ type User struct {
 }
 
 type TweetData struct {
-	CreatedAt     time.Time
-	AuthorID      string
-	ID            string
-	Text          string
-	PublicMetrics map[int64]map[string]int
+	CreatedAt time.Time
+	AuthorID  string
+	ID        string
+	Text      string
+	Data      map[int64]entities.TweetPublicMetrics
 }
 
 var tweet_authors = map[string]string{}
+var found_tweets = []string{}
 var bot *twigo.Client
 var db *scribble.Driver
 var scheduler *gocron.Scheduler
 
 func main() {
-	var bearer_token string
 	var err error
 
-	flag.StringVar(&bearer_token, "bearer", "", "Bearer token")
+	bearer_token := flag.String("bearer", "", "Bearer token")
+	flag.Parse()
 
-	bot, err = twigo.NewClient("", "", "", "", bearer_token)
+	bot, err = twigo.NewClient("", "", "", "", *bearer_token)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -49,10 +51,10 @@ func main() {
 
 	app := fiber.New()
 
-	app.Post("/add_users_to_be_tracked", func(c *fiber.Ctx) error {
+	app.Post("/add_users_to_be_tracked", func(ctx *fiber.Ctx) error {
 		var user_ids []string
 
-		if err := c.BodyParser(&user_ids); err != nil {
+		if err := ctx.BodyParser(&user_ids); err != nil {
 			return err
 		}
 
@@ -65,53 +67,100 @@ func main() {
 			}
 		}
 
-		return c.JSON(true)
+		return ctx.JSON(true)
+	})
+
+	app.Post("/stop_tracking_users", func(ctx *fiber.Ctx) error {
+		var user_ids []string
+
+		if err := ctx.BodyParser(&user_ids); err != nil {
+			return err
+		}
+
+		for _, user_id := range user_ids {
+			if err := scheduler.RemoveByTag(fmt.Sprintf("user_track_%s", user_id)); err == nil {
+				fmt.Printf("tracking user %s stopped\n", user_id)
+			}
+		}
+		return ctx.JSON(true)
+	})
+
+	app.Post("/stop_tracking_tweets", func(ctx *fiber.Ctx) error {
+		var tweet_ids []string
+
+		if err := ctx.BodyParser(&tweet_ids); err != nil {
+			return err
+		}
+
+		for _, tweet_id := range tweet_ids {
+			if err := scheduler.RemoveByTag(fmt.Sprintf("tweet_track_%s", tweet_id)); err == nil {
+				fmt.Printf("tracking tweet %s stopped\n", tweet_id)
+			}
+		}
+		return ctx.JSON(true)
 	})
 
 	scheduler.StartAsync()
 	log.Fatal(app.Listen(":3000"))
 }
 
-func CollectTweetData(tweet_id string) {
+func TrackTweet(tweet_id string) {
 	fields := map[string]interface{}{"tweet.fields": []string{"author_id", "created_at", "public_metrics"}}
-	response, _ := bot.GetTweet(tweet_id, false, fields)
+	response, err := bot.GetTweet(tweet_id, fields)
+	if err != nil {
+		fmt.Println(err)
+	}
 	tweet := response.Data
-	fmt.Printf("Collection tweet data: %s\n", tweet_id)
-	item_id := fmt.Sprintf("%s_%s", tweet.AuthorID, tweet_id)
 	if tweet.ID != "" {
-		if _, ok := tweet_authors[tweet_id]; ok {
-			tweet_data := &TweetData{}
-			db.Read("users", item_id, tweet_data)
-			fmt.Printf("%#v\n", tweet_data)
-			tweet_data.PublicMetrics[time.Now().Unix()] = tweet.PublicMetrics
-			db.Write("users", item_id, tweet_data)
-		} else {
-			tweet_data := &TweetData{
-				CreatedAt:     tweet.CreatedAt,
-				AuthorID:      tweet.AuthorID,
-				ID:            tweet.ID,
-				Text:          tweet.Text,
-				PublicMetrics: map[int64]map[string]int{},
-			}
-			tweet_data.PublicMetrics[time.Now().Unix()] = tweet.PublicMetrics
-			db.Write("users", item_id, tweet_data)
-			tweet_authors[tweet_id] = tweet.AuthorID
-		}
+		CollectTweetData(tweet)
 	}
 }
 
 func TrackUser(user_id string) {
 	start_time := time.Now().UTC().Add(-5 * time.Minute)
 	params := map[string]interface{}{"max_results": 5, "start_time": start_time, "tweet.fields": []string{"created_at"}}
-	user_tweets, _ := bot.GetUserTweets(user_id, false, params)
+	user_tweets, _ := bot.GetUserTweets(user_id, params)
 	// user_tweets, _ := testGetUserTweets(user_id, false, params)
 	if len(user_tweets.Data) != 0 { // It's surely not nil
-		fmt.Printf("A tweet found: https://twitter.com/i/web/status/%s\n", user_tweets.Data[0].ID)
-		scheduler.Every(5).Seconds().Do(CollectTweetData, user_tweets.Data[0].ID)
-		if err := scheduler.RemoveByTag(fmt.Sprintf("user_track_%s", user_id)); err == nil {
-			fmt.Printf("tracking user %s stopped\n", user_id)
+		tweet_id := user_tweets.Data[0].ID
+		if !contains(found_tweets, tweet_id) {
+			fmt.Printf("A tweet found: https://twitter.com/i/web/status/%s\n", tweet_id)
+			scheduler.Every(5).Seconds().Tag(fmt.Sprintf("tweet_track_%s", tweet_id)).Do(TrackTweet, tweet_id)
+			found_tweets = append(found_tweets, tweet_id)
 		}
 	}
+}
+
+func CollectTweetData(tweet entities.Tweet) {
+	fmt.Printf("Collecting tweet data: %s\n", tweet.ID)
+	item_id := fmt.Sprintf("tweet_%s_written_by_%s", tweet.ID, tweet.AuthorID)
+	if _, ok := tweet_authors[tweet.ID]; ok {
+		tweet_data := &TweetData{}
+		db.Read("users", item_id, tweet_data)
+		fmt.Printf("%#v\n", tweet_data)
+		tweet_data.Data[time.Now().Unix()] = tweet.PublicMetrics
+		db.Write("users", item_id, tweet_data)
+	} else {
+		tweet_data := &TweetData{
+			CreatedAt: tweet.CreatedAt,
+			AuthorID:  tweet.AuthorID,
+			ID:        tweet.ID,
+			Text:      tweet.Text,
+			Data:      map[int64]entities.TweetPublicMetrics{},
+		}
+		tweet_data.Data[time.Now().Unix()] = tweet.PublicMetrics
+		db.Write("users", item_id, tweet_data)
+		tweet_authors[tweet.ID] = tweet.AuthorID
+	}
+}
+
+func contains(arrayOfStrings []string, string_item string) bool {
+	for _, val := range arrayOfStrings {
+		if val == string_item {
+			return true
+		}
+	}
+	return false
 }
 
 // func testGetUserTweets(user_id string, oauth_1a bool, params map[string]interface{}) (*twigo.TweetsResponse, error) {
