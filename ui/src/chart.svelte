0484d46036880134fb5325b6e4e3cd35528a5f8b
arshamalh <arshamalh@gmail.com>
2022-05-12 11:20:59 +0430

    more cleaner code for chart.

--- ui/src/chart.svelte
+++ ui/src/chart.svelte
@@ -4,53 +4,40 @@
 
 	export let jsonData = {}
 	let chartCanvas
-	let labels = []
-	let like_data = []
-	let retweet_data = []
-	let reply_data = []
-	let quote_data = []
 	let chart
-
-	function resetData () {
-		chart.reset()
-		labels = []
-		like_data = []
-		retweet_data = []
-		reply_data = []
-		quote_data = []
-	}
 
 	const data = {
 		labels: labels,
-		datasets: [{
-			label: 'Likes',
-			data: like_data,
-			fill: false,
-			borderColor: 'rgb(75, 192, 192)',
-			tension: 0.1
-		},
-		{
-			label: 'Retweets',
-			data: retweet_data,
-			fill: false,
-			borderColor: 'rgb(255, 99, 132)',
-			tension: 0.1
-		},
-		{
-			label: 'Quotes retweets',
-			data: quote_data,
-			fill: false,
-			borderColor: 'rgb(255, 159, 64)',
-			tension: 0.1
-		},
-		{
-			label: 'Replies',
-			data: reply_data,
-			fill: false,
-			borderColor: 'rgb(255, 205, 86)',
-			tension: 0.1
-		}
-	]
+		datasets: [
+			{
+				label: 'Likes',
+				data: [],
+				fill: false,
+				borderColor: 'rgb(75, 192, 192)',
+				tension: 0.1
+			},
+			{
+				label: 'Retweets',
+				data: [],
+				fill: false,
+				borderColor: 'rgb(255, 99, 132)',
+				tension: 0.1
+			},
+			{
+				label: 'Quotes retweets',
+				data: [],
+				fill: false,
+				borderColor: 'rgb(255, 159, 64)',
+				tension: 0.1
+			},
+			{
+				label: 'Replies',
+				data: [],
+				fill: false,
+				borderColor: 'rgb(255, 205, 86)',
+				tension: 0.1
+			}
+		]
 	};
 
 	onMount(() => {
@@ -63,17 +50,20 @@
 				responsive: true,
 			}
 		});
-        console.log(jsonData)
-        console.log(labels)
-        console.log("Log me!")
-        for (const key of Object.keys(jsonData)) {
-            labels = labels.concat(key)
-            like_data = like_data.concat(jsonData[key]["like_count"])
-            retweet_data = retweet_data.concat(jsonData[key]["retweet_count"])
-            reply_data = reply_data.concat(jsonData[key]["reply_count"])
-            quote_data = quote_data.concat(jsonData[key]["quote_count"])
+		let labels = []
+		let like_data = []
+		let retweet_data = []
+		let reply_data = []
+		let quote_data = []
+		let timeStamps = Object.keys(jsonData)
+		let first_ts = timeStamps[0]
+        for (const ts of timeStamps) {
+            labels.push(Math.round((parseInt(ts) - parseInt(first_ts))/60))
+            like_data.push(jsonData[ts]["like_count"])
+            retweet_data.push(jsonData[ts]["retweet_count"])
+            reply_data.push(jsonData[ts]["reply_count"])
+            quote_data.push(jsonData[ts]["quote_count"])
         }
-        console.log(labels)
         chart.data.labels = labels
         chart.data.datasets[0].data = like_data
         chart.data.datasets[1].data = retweet_data
